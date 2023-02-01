a3e4087be280e9422783a178954ac81b855ed52b
unknown <mari-jen@hotmail.com>
2023-02-01 16:19:28 -0300

    first commit

---
+++ endpoints/usuario_post.php
@@ -0,0 +1,50 @@
+<?php
+
+function api_usuario_post($request) {
+  $nome = sanitize_text_field($request['nome']);
+  $email = sanitize_email($request['email']);
+  $senha = $request['senha'];
+  $rua = sanitize_text_field($request['rua']);
+  $cep = sanitize_text_field($request['cep']);
+  $numero = sanitize_text_field($request['numero']);
+  $cidade = sanitize_text_field($request['cidade']);
+  $estado = sanitize_text_field($request['estado']);
+
+  
+  $user_exists = username_exists($email);
+  $email_exists = email_exists($email);
+  
+  if(!$user_exists && !$email_exists && $email && $senha) {
+    $user_id = wp_create_user($email, $senha, $email);
+
+    $response = array(
+      'ID' => $user_id, //o ID é um número único criado pelo wordpress que retorna da função wp_create_user
+      'display_name' => $nome,
+      'first_name' => $nome,
+      'role' => 'subscriber',
+    );
+    wp_update_user($response);
+    update_user_meta($user_id, 'cep', $cep);
+    update_user_meta($user_id, 'rua', $rua);
+    update_user_meta($user_id, 'numero', $numero);
+    update_user_meta($user_id, 'cidade', $cidade);
+    update_user_meta($user_id, 'estado', $estado);
+  } else {
+    $response = new WP_Error('email', 'Email já cadastrado.', array('status' => 403));
+  }
+  return rest_ensure_response($response);
+}
+
+function registrar_api_usuario_post() {
+  register_rest_route('api', '/usuario', array(
+    array(
+      'methods' => WP_REST_Server::CREATABLE,
+      'callback' => 'api_usuario_post',
+    ),
+  ));
+}
+
+add_action('rest_api_init', 'registrar_api_usuario_post');
+
+
+?>
